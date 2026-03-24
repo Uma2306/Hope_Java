6ae7bd7a8e96648fa1f5f7b44cd65666d534ec62
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 21:26:15 +0530

    Day2 NumberOfCuts.java

---
+++ Day2/NumberOfCuts.java
@@ -0,0 +1,14 @@
+Leetcode 2481: https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n==1)
+        {
+             return 0;
+        }
+        if (n%2==0) 
+        return n/2;
+        else 
+        return n;
+        
+    }
+}
