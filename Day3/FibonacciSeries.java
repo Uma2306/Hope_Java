00b8d3f2b57312d6d5e522e3c300d988a7ed5738
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:11:23 +0530

    Day3 FibonacciSeries.java

---
+++ Day3/FibonacciSeries.java
@@ -0,0 +1,16 @@
+Leetcode 509: https://leetcode.com/problems/fibonacci-number/
+class Solution {
+    public int fib(int n) {
+        if(n<=1) return n;
+       int a=0;
+       int b=1;
+       for(int i=2;i<=n;i++)
+       {
+        int c=a+b;
+        a=b;
+        b=c;
+       }
+       return b;
+
+    }
+}
