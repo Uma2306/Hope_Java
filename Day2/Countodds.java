55489b43e3a5775541efbca495bfb53d01b2911f
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 21:30:36 +0530

    Day2 Countodds.java

---
+++ Day2/Countodds.java
@@ -0,0 +1,10 @@
+Leetcode 1523: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+class Solution {
+    public int countOdds(int low, int high) {
+        int n=(high-low)+1;
+        if(low%2!=0 && high%2!=0) return n/2+1;
+        else
+         return n/2;
+        
+    }
+}
