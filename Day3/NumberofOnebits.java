1f31daf79e32b81c0b473a122d60774377eeed17
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:09:55 +0530

    Day3 NumberofOnebits.java

---
+++ Day3/NumberofOnebits.java
@@ -0,0 +1,12 @@
+Leetcode 191: https://leetcode.com/problems/number-of-1-bits/
+class Solution {
+    public int hammingWeight(int n) {
+    int count=0;
+    while(n!=0)
+    {
+        n=n&(n-1);
+        count++;
+    }
+    return count;
+    }
+}
