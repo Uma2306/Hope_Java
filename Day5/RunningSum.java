e61f7c72b3149e39d7f6c30f5b8b1cfcdc694e6d
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 23:22:09 +0530

    Day5 RunningSum.java

---
+++ Day5/RunningSum.java
@@ -0,0 +1,16 @@
+Leetcode 1480: https://leetcode.com/problems/running-sum-of-1d-array/
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+
+            sum=sum+nums[i];
+            nums[i]=sum;
+        }
+        return nums;
+        
+         
+    }  
+    
+}
