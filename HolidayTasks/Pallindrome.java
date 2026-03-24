f78b6daa7df0f239f0aa65c37d4f1b4e13510ae9
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:29:14 +0530

    Task5 Pallindrome.java

---
+++ HolidayTasks/Pallindrome.java
@@ -0,0 +1,15 @@
+Leetcode 9: https://leetcode.com/problems/palindrome-number/
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int org=x;
+        int rev=0;
+        while(x!=0)
+        {
+            rev=rev*10+x%10;
+            x=x/10;
+        }
+        return org==rev;
+        
+    }
+}
