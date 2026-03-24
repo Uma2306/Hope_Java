3dd836557b999739015ac3dc8460947f2e53cf83
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 21:27:45 +0530

    Day2 DivisorGame.java

---
+++ Day2/DivisorGame.java
@@ -0,0 +1,8 @@
+Leetcode 1025: https://leetcode.com/problems/divisor-game/
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0) return true;
+        else return false;
+        
+    }
+}
