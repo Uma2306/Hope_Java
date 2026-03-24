e8f51a505aa572078228f321b5dc4b031663cbea
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:06:29 +0530

    Day2 Singlenumber.java

---
+++ Day2/Singlenumber.java
@@ -0,0 +1,11 @@
+Leetcode 136: https://leetcode.com/problems/single-number/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res=0;
+        for(int num:nums){
+            res^=num;
+
+        }
+        return res;
+    }
+}
