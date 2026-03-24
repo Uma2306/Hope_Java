49d07249b8e999974c8721846c53ae5263218ed4
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:52:56 +0530

    Day4 Watterbottle.java

---
+++ Day4/Watterbottle.java
@@ -0,0 +1,7 @@
+Leetcode 1518: https://leetcode.com/problems/water-bottles/
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        return numBottles+(numBottles-1)/(numExchange-1);
+        
+    }
+}
