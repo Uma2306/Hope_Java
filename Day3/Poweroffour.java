b6e58f6f457bae1f23ec82c5c3e6aa55401414c9
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:08:36 +0530

    Day3 Poweroffour.java

---
+++ Day3/Poweroffour.java
@@ -0,0 +1,7 @@
+Leetcode 342: https://leetcode.com/problems/power-of-four/
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return n>0 && (n&(n-1))==0 && (n&0x55555555)!=0;
+        
+    }
+}
