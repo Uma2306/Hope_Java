d0f509a222c1f252bffb61e35caa71ee3d3b3a5d
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 22:35:49 +0530

    Task9 Threedivisors.java

---
+++ HolidayTasks/Threedivisors.java
@@ -0,0 +1,12 @@
+Leetcode 1952: https://leetcode.com/problems/three-divisors/
+class Solution {
+    public boolean isThree(int n) {
+        int count=2;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0) count++;
+        }
+        if(count==3) return true;
+        else return false;
+    }
+}
