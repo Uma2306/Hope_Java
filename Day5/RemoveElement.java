67954f594e00b57199a9a554f7cdeba4abcc6053
Uma2306 <umamurugesan2306@gmail.com>
2026-03-24 23:19:54 +0530

    Day5 RemoveElement.java

---
+++ Day5/RemoveElement.java
@@ -0,0 +1,16 @@
+Leetcode 3232 : https://leetcode.com/problems/remove-element/
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i=0;
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]!=val)
+            {
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        return i;
+        
+    }
+}
